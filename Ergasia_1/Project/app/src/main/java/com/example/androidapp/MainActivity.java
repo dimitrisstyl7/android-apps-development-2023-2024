52adc546b6b7e0336b66abe1ffc2b17659c0b261
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-04 14:13:08 +0200

    Project updated.

--- Ergasia_1/Project/app/src/main/java/com/example/androidapp/MainActivity.java
+++ Ergasia_1/Project/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.androidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private TextView speed_textView;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        speed_textView = findViewById(R.id.speed_textView);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    public void getSpeed(View view) {
+        // Check if the permission has already been granted.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // If granted, register for location updates.
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        } else {
+            // If not granted, request the permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        speed_textView.setText(String.format("%s m/s", location.getSpeed()));
     }
 }
