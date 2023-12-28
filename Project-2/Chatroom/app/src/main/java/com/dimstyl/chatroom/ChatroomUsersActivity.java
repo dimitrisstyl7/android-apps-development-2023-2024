6ea0f27dda63d3229cf98b57bc5f6aa6b9b83434
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-28 19:14:55 +0200

    rename ChatroomActivity.java to ChatroomUsersActivity.java

--- Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/ChatroomUsersActivity.java
+++ Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/ChatroomUsersActivity.java
@@ -1,14 +1,13 @@
 package com.dimstyl.chatroom;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class ChatroomActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class ChatroomUsersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chatroom);
+        setContentView(R.layout.activity_chatroom_users);
     }
 }
