f48baecc245a10449c71c5629e533f5778078b7b
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-31 11:37:12 +0200

    update onRestart method (MainActivity.java)

--- Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/MainActivity.java
+++ Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+        FirebaseUtil.signOut();
         clearTextFields();
     }
 
