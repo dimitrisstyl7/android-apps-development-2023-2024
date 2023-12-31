ffeac5c06309fcfcb23ccc98ed8f414c2d97c7b3
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-31 11:47:27 +0200

    update onCreate method (ChatroomActivity.java)

--- Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/ChatroomActivity.java
+++ Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/ChatroomActivity.java
@@ -1,8 +1,10 @@
 package com.dimstyl.chatroom;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ChatroomActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class ChatroomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatroom);
+        TextView chattingWithtextView = findViewById(R.id.chattingWithTextView);
+
+        String receiverUid = getIntent().getStringExtra("receiverUid");
+
+        if (receiverUid == null) {
+            showMessage("Error", "Something went wrong, please try again.");
+            return;
+        }
+
+        String receiverNickname =
+                receiverUid.equals(FirebaseUtil.getUid()) ? "yourself" : getIntent().getStringExtra("receiverNickname");
+        chattingWithtextView.setText(getString(R.string.chatting_with, receiverNickname));
+    }
+
+    void showMessage(String title, String message) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setCancelable(true)
+                .show();
     }
 }
