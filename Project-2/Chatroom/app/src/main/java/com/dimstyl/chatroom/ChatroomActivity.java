6a32df2951049a5f30fdb90845ba6b3a3d30466b
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-01-01 13:58:02 +0200

    update onCreate method (ChatroomActivity.java)

--- Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/ChatroomActivity.java
+++ Project-2/Chatroom/app/src/main/java/com/dimstyl/chatroom/ChatroomActivity.java
@@ -1,29 +1,45 @@
 package com.dimstyl.chatroom;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChatroomActivity extends AppCompatActivity {
+    private EditText inputEditText;
+    private LinearLayout messagesLinearLayout;
+    private String receiverUid;
+    private String receiverNickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatroom);
+
+        messagesLinearLayout = findViewById(R.id.messagesLinearLayout);
         TextView chattingWithtextView = findViewById(R.id.chattingWithTextView);
+        inputEditText = findViewById(R.id.inputEditText);
 
-        String receiverUid = getIntent().getStringExtra("receiverUid");
-
+        receiverUid = getIntent().getStringExtra("receiverUid");
         if (receiverUid == null) {
-            showMessage("Error", "Something went wrong, please try again.");
+            showMessage("Oops...", "Something went wrong, please try again.", "Close", (dialogInterface, i) -> finish());
             return;
         }
 
-        String receiverNickname =
+        receiverNickname =
                 receiverUid.equals(FirebaseUtil.getUid()) ? "yourself" : getIntent().getStringExtra("receiverNickname");
         chattingWithtextView.setText(getString(R.string.chatting_with, receiverNickname));
+
+        /*
+         * Add listener for messages.
+         * At the beginning, it will fill linearLayout with old messages.
+         * After that, it will add new messages to linearLayout.
+         * */
+        FirebaseUtil.addChildEventListener(receiverUid, this);
     }
 
     void showMessage(String title, String message) {
